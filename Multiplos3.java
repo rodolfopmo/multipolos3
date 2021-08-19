80d1e1b450979130c55ef6d6977d2a03cb615abb
rodolfopmo <rodolfo.pmo@gmail.com>
2021-08-19 20:19:59 +0000

    Código Exercício 3

---
+++ Multiplos3.java
@@ -0,0 +1,8 @@
+public class Multiplos3{
+    public static void main(String args[]){
+        for(int i = 1; i <= 100; i ++){
+            if(i % 3 == 0)
+                System.out.println("Os números de 1 a 100 múltiplos de 3 são: " + i);
+        }
+    }
+}
